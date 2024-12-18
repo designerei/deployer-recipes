9725fbdec5ddb5c791cfc330be2999a016ebe8d8
Hendrik Bengs <hben@designerei-werbeagentur.de>
2024-12-18 15:10:59 +0100

    Added task to generate fake app.output.css file

--- recipe/assets.php
+++ recipe/assets.php
@@ -15,5 +15,15 @@ task('assets:clear', function () {
     runLocally('rm -rf ./{{public_path}}/asset-mapper');
 });
 
+// generate fake app.output.css file
+desc('Added app.output.css');
+task('generate:input-css', function () {
+    cd('{{release_path}}');
+    run('mkdir {{release_path}}/var/sass');
+    cd('{{release_path}}/var/sass');
+    run('touch app.output.css');
+});
+
 before('deploy', 'assets:build');
+after('deploy', 'generate:input-css');
 after('deploy', 'assets:clear');
